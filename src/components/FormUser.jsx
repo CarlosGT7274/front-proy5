e75374cffa1c0821e3a681d06c006a8c12595fa2
CarlosGT7274 <carlosbt7274@gmail.com>
2023-04-29 22:21:57 -0600

    Update FormUser.jsx

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -9,7 +9,7 @@ const FormUser = () => {
      const [userData,setData] = useState()
 
      const saveUser = async() =>{
-      const url = 'http://localhost:4000/api/v1/register'
+      const url = 'https://back-proy5-9ks61nrs9-carlosgt7274.vercel.app/api/v1/register'
       const result = await axios.post(url, userData)
       console.log(result)
      }
