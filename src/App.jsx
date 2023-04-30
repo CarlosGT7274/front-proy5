b7b13bc9f5b7273cb07756a59b323c1360d089f6
CarlosGT7274 <carlosbt7274@gmail.com>
2023-04-29 22:47:46 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import IndexLayout from './components/layouts'
 function App() {
   const content = (
     <div>
-      <h1>hola</h1>
+      <main>
+        <p>En nuestra tienda encontrarás una gran variedad de productos de alta calidad a precios competitivos. Desde tecnología hasta moda, pasando por belleza, hogar y mucho más.</p>
+      </main>
+      <footer>
+        <p>&copy; 2023 Ecommerce Example. Todos los derechos reservados.</p>
+      </footer>
     </div>
   )
 
